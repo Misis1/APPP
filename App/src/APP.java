23c66dcb83e5b9d814146ba2a908a3c62c0c29ed
Misis1 <heikki.saarni@hotmail.com>
2022-04-05 12:54:02 +0300

    Initial commit

---
+++ App/src/APP.java
@@ -0,0 +1,8 @@
+public class APP {
+    public static void main(String[] args){
+        int myNumber = 88;
+
+        short myShort = 847;
+        System.out.println(myNumber);
+    }
+}
---
+++ App/src/application.java
@@ -0,0 +1,6 @@
+class applications {
+    public static void main(String[] args) {
+        System.out.println(testtest);
+
+    }
+}
